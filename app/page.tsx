8c84ca565dab7791c68fe6ce54de518672f291f3
chris18011999 <chris18011999@gmail.com>
2024-11-05 13:42:46 +0100

    add homepage sidebartrigger

--- app/page.tsx
+++ app/page.tsx
@@ -8,45 +8,50 @@ import * as motion from "framer-motion/client";
 import { AnimatePresence } from "framer-motion";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import type { Metadata } from "next";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 const components = [CurrentTime, WeatherInfo, BatteryInfo, QuoteOfTheDay];
 
 export const metadata: Metadata = {
   title: "Dashboard",
-}
+};
 
 export default function HomePage() {
   return (
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {components.map((Component, index) => {
-        return (
-          <AnimatePresence key={Component.name}>
-            <motion.div
-              transition={{ delay: 0.05 * index }}
-              variants={{
-                show: { y: 0, opacity: 1 },
-                hide: { y: -10, opacity: 0.3 },
-              }}
-              exit="hide"
-              animate="show"
-              initial="hide"
-            >
-              <Suspense
-                fallback={
-                  <Card>
-                    <CardHeader>
-                      <CardTitle>Component Loading</CardTitle>
-                    </CardHeader>
-                    <CardContent>Loading...</CardContent>
-                  </Card>
-                }
+    <div className="flex flex-col gap-3">
+      <SidebarTrigger size={"icon"} />
+
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {components.map((Component, index) => {
+          return (
+            <AnimatePresence key={Component.name}>
+              <motion.div
+                transition={{ delay: 0.05 * index }}
+                variants={{
+                  show: { y: 0, opacity: 1 },
+                  hide: { y: -10, opacity: 0.3 },
+                }}
+                exit="hide"
+                animate="show"
+                initial="hide"
               >
-                <Component />
-              </Suspense>
-            </motion.div>
-          </AnimatePresence>
-        );
-      })}
+                <Suspense
+                  fallback={
+                    <Card>
+                      <CardHeader>
+                        <CardTitle>Component Loading</CardTitle>
+                      </CardHeader>
+                      <CardContent>Loading...</CardContent>
+                    </Card>
+                  }
+                >
+                  <Component />
+                </Suspense>
+              </motion.div>
+            </AnimatePresence>
+          );
+        })}
+      </div>
     </div>
   );
 }
